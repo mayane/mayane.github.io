7f4f20ebbb8251af6a2546219f6d6b213a05f356
Marianne LO <mayane.lo@gmail.com>
2016-01-13 11:40:09 +0100

    regrouper les scripts js dans main.js et debug pour small devices

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,130 @@
-console.log('This would be the main JS file.');
+// Scrollspy fluide
+$(function () {
+    $('header a').on('click', function (e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000, function () {
+            window.location.hash = hash;
+        });
+    });
+});
+
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: FR (French; français)
+ */
+$.extend($.validator.messages, {
+    required: "Ce champ est obligatoire.",
+    remote: "Veuillez corriger ce champ.",
+    email: "Veuillez fournir une adresse électronique valide.",
+    url: "Veuillez fournir une adresse URL valide.",
+    date: "Veuillez fournir une date valide.",
+    dateISO: "Veuillez fournir une date valide (ISO).",
+    number: "Veuillez fournir seulement des chiffres.",
+    digits: "Veuillez fournir seulement des chiffres.",
+    creditcard: "Veuillez fournir un numéro de carte de crédit valide.",
+    equalTo: "Veuillez fournir encore la mÃªme valeur.",
+    extension: "Veuillez fournir une valeur avec une extension valide.",
+    maxlength: $.validator.format("Veuillez fournir au plus {0} caractères."),
+    minlength: $.validator.format("Veuillez fournir au moins {0} caractères."),
+    rangelength: $.validator.format("Veuillez fournir une valeur qui contient entre {0} et {1} caractères."),
+    range: $.validator.format("Veuillez fournir une valeur entre {0} et {1}."),
+    max: $.validator.format("Veuillez fournir une valeur inférieure ou égale Ã  {0}."),
+    min: $.validator.format("Veuillez fournir une valeur supérieure ou égale Ã  {0}."),
+    maxWords: $.validator.format("Veuillez fournir au plus {0} mots."),
+    minWords: $.validator.format("Veuillez fournir au moins {0} mots."),
+    rangeWords: $.validator.format("Veuillez fournir entre {0} et {1} mots."),
+    letterswithbasicpunc: "Veuillez fournir seulement des lettres et des signes de ponctuation.",
+    alphanumeric: "Veuillez fournir seulement des lettres, nombres, espaces et soulignages.",
+    lettersonly: "Veuillez fournir seulement des lettres.",
+    nowhitespace: "Veuillez ne pas inscrire d'espaces blancs.",
+    ziprange: "Veuillez fournir un code postal entre 902xx-xxxx et 905-xx-xxxx.",
+    integer: "Veuillez fournir un nombre non décimal qui est positif ou négatif.",
+    vinUS: "Veuillez fournir un numéro d'identification du véhicule (VIN).",
+    dateITA: "Veuillez fournir une date valide.",
+    time: "Veuillez fournir une heure valide entre 00:00 et 23:59.",
+    phoneUS: "Veuillez fournir un numéro de téléphone valide.",
+    phoneUK: "Veuillez fournir un numéro de téléphone valide.",
+    mobileUK: "Veuillez fournir un numéro de téléphone mobile valide.",
+    strippedminlength: $.validator.format("Veuillez fournir au moins {0} caractères."),
+    email2: "Veuillez fournir une adresse électronique valide.",
+    url2: "Veuillez fournir une adresse URL valide.",
+    creditcardtypes: "Veuillez fournir un numéro de carte de crédit valide.",
+    ipv4: "Veuillez fournir une adresse IP v4 valide.",
+    ipv6: "Veuillez fournir une adresse IP v6 valide.",
+    require_from_group: "Veuillez fournir au moins {0} de ces champs.",
+    nifES: "Veuillez fournir un numéro NIF valide.",
+    nieES: "Veuillez fournir un numéro NIE valide.",
+    cifES: "Veuillez fournir un numéro CIF valide.",
+    postalCodeCA: "Veuillez fournir un code postal valide."
+});
+
+$.validator.addMethod("telephoneformat", function (value, element) {
+    return this.optional(element) || /^[0-9]{10}$/i.test(value);
+}, "Le téléphone doit être composés de 10 chiffres avec aucun espace et caractères spéciaux.");
+
+//FORMULAIRE
+$("#monformulaire").validate({
+    submitHandler: function (form) {
+        var serializedData = $("#monformulaire").serialize();
+
+        $.ajax({
+            url: 'http://mayane.shost.ca/envoyerMail.php',
+            dataType: 'json',
+            cache: false,
+            type: 'POST',
+            data: serializedData,
+            success: function (response) {
+                if (response.status == 'success') {
+                    $('#resultat').removeClass("has-error");
+                    $('#resultat').addClass("has-success");
+                    $('html, body').animate({
+                        scrollTop: $('#resultat').offset().top
+                    }, 0);
+                    $('#resultat').html("<p>" + response.message + "</p>");
+                } else {
+                    $('#resultat').removeClass("has-success");
+                    $('#resultat').addClass("has-error");
+                    $('html, body').animate({
+                        scrollTop: $('#resultat').offset().top
+                    }, 0);
+                    $('#resultat').html("<p>" + response.message + "</p>");
+                }
+            }
+        });
+    },
+    rules: {
+        telephone: {
+            telephoneformat: true
+        }
+    },
+    errorElement: "span",
+    errorClass: 'help-block col-md-12',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent().parent());
+        } else {
+            error.insertAfter(element);
+        }
+    },
+    highlight: function (element) {
+        $(element).closest('.form-group').addClass('has-error');
+        $(element).closest('.form-group').removeClass('has-success');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-error');
+
+        if (!$(element).has("non-requis")) {
+            $(element).closest('.form-group').addClass('has-success');
+        }
+
+        if ($(element).has("non-requis")) {
+            if ($(element).val() != '') {
+                $(element).closest('.form-group').addClass('has-success');
+            }
+        }
+    }
+
+});
